8a97b1c55abad4fc2bf20e69529a38fd8d5fe9ee
Erfan Momeni <Erfan.Momeni@snapp.cab>
2022-07-04 22:52:04 +0430

    add model for connect to cassandra

--- models/db_model.php
+++ models/db_model.php
@@ -3,18 +3,14 @@ class db_model {
 
     public function __construct()
     {
-        $cluster   = Cassandra::cluster()                 // connects to localhost by default
-        ->build();
+        $cluster   = Cassandra::cluster()
+            ->build();
         $keyspace  = 'system';
-        $session   = $cluster->connect($keyspace);        // create session, optionally scoped to a keyspace
-        $statement = new Cassandra\SimpleStatement(       // also supports prepared and batch statements
+        $session   = $cluster->connect($keyspace);
+        $statement = new Cassandra\SimpleStatement(
             'SELECT keyspace_name, columnfamily_name FROM schema_columnfamilies'
         );
-        $future    = $session->executeAsync($statement);  // fully asynchronous and easy parallel execution
-        $result    = $future->get();                      // wait for the result, with an optional timeout
-
-        foreach ($result as $row) {                       // results and rows implement Iterator, Countable and ArrayAccess
-            printf("The keyspace %s has a table called %s\n", $row['keyspace_name'], $row['columnfamily_name']);
-        }
+        $future    = $session->executeAsync($statement);
+        $result    = $future->get();
     }
 }
